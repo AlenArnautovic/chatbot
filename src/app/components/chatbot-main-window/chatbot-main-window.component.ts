18fa9747f431043ee5e7b246fb1de6b1bb814574
Nicolai Haferkamp <nicolai.haferkamp@web.de>
2023-01-08 21:17:40 +0100

    feat: added typing 'function' of chatbot

--- src/app/components/chatbot-main-window/chatbot-main-window.component.ts
+++ src/app/components/chatbot-main-window/chatbot-main-window.component.ts
@@ -13,6 +13,10 @@ import { Router } from '@angular/router';
 })
 export class ChatbotMainWindowComponent implements OnInit {
   panelHeader = "Chatbot";
+  isTypingContent = 'Chatbot is typing...'
+  
+  showIsTyping = false;
+
   inputFieldValue!: string; 
   items!: MenuItem[];
   messageObjects: MessageObject[] = [];
@@ -55,12 +59,25 @@ export class ChatbotMainWindowComponent implements OnInit {
   async onSendMessage(){
     if(this.inputFieldValue.trim().length >0){
       this.createMessage(this.inputFieldValue, false);
+      this.showIsTyping = true;
       const response = await this.communicationService.sendMessageToDialogFlow(this.inputFieldValue);
+      this.wait(800);
+      this.showIsTyping = false;
       this.createMessage(response.fulfillmentText,true);
     }
     this.inputFieldValue = "";
   }
 
+  //TODO maybe rework this fuction because it pauses the whole code
+  //https://stackoverflow.com/questions/14226803/wait-5-seconds-before-executing-next-line
+  wait(ms:number){
+    const start = new Date().getTime();
+    let end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
+ }
+ 
 changeThemeForExistingMessages(){
   const currentTheme = this.themeSerivce.getCurrentTheme();
   this.messageObjects.forEach(element => {
